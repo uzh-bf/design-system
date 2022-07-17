4681649701857e932339304e494a16e689deb5d1
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-07-17 18:08:40 +0200

    feat(Header): add basic header components

---
+++ src/Header.stories.tsx
@@ -0,0 +1,14 @@
+import Header from './Header'
+
+export const Default = () => (
+  <div>
+    <Header.H1>asdasd</Header.H1>
+    <p>hello world</p>
+    <Header.H2>asdasd</Header.H2>
+    <p>hello world</p>
+    <Header.H3>asdasd</Header.H3>
+    <p>hello world</p>
+    <Header.H4>asdasd</Header.H4>
+    <p>hello world</p>
+  </div>
+)
---
+++ src/Header.tsx
@@ -0,0 +1,65 @@
+import { twMerge } from 'tailwind-merge'
+
+interface HeaderProps {
+  className?: string
+  children: React.ReactNode
+}
+
+export function H1({ className, children }: HeaderProps) {
+  return (
+    <h1
+      className={twMerge(
+        'mb-[0.2em] text-2xl font-bold font-thesans',
+        className
+      )}
+    >
+      {children}
+    </h1>
+  )
+}
+
+export function H2({ className, children }: HeaderProps) {
+  return (
+    <h2
+      className={twMerge(
+        'mb-[0.2em] text-xl font-bold font-thesans',
+        className
+      )}
+    >
+      {children}
+    </h2>
+  )
+}
+
+export function H3({ className, children }: HeaderProps) {
+  return (
+    <h3
+      className={twMerge(
+        'mb-[0.2em] text-lg font-bold font-thesans',
+        className
+      )}
+    >
+      {children}
+    </h3>
+  )
+}
+
+export function H4({ className, children }: HeaderProps) {
+  return (
+    <h4
+      className={twMerge(
+        'mb-[0.2em] font-bold text-md font-thesans',
+        className
+      )}
+    >
+      {children}
+    </h4>
+  )
+}
+
+export default {
+  H1,
+  H2,
+  H3,
+  H4,
+}
