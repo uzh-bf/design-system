103d44b03711e2ed018a245802199d1be454ed4b
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-07-23 21:35:51 +0200

    enhance: upgrade packages and add tailwindcss-radix

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
+    require('tailwindcss-radix')({ variantPrefix: 'rdx' }),
   ],
 }
