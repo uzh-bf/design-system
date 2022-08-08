5b59b34f7647371b029efba9be458e08bf0cb8d5
florinavogel <florina.vogel@gmail.com>
2022-08-08 17:02:48 +0200

    Add navbar component: First working version

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,57 @@ module.exports = {
       fontFamily: {
         ...TailwindFonts,
       },
+      keyframes: {
+        "enter-from-right": {
+          "0%": { transform: "translateX(200px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        "enter-from-left": {
+          "0%": { transform: "translateX(-200px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        },
+        "exit-to-right": {
+          "0%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(200px)", opacity: 0 },
+        },
+        "exit-to-left": {
+          "0%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(-200px)", opacity: 0 },
+        },
+        "scale-in-content": {
+          "0%": { transform: "rotateX(-30deg) scale(0.9)", opacity: 0 },
+          "100%": { transform: "rotateX(0deg) scale(1)", opacity: 1 },
+        },
+        "scale-out-content": {
+          "0%": { transform: "rotateX(0deg) scale(1)", opacity: 1 },
+          "100%": { transform: "rotateX(-10deg) scale(0.95)", opacity: 0 },
+        },
+        "fade-in": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        "fade-out": {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+      },
+      animation: {
+        "enter-from-right": "enter-from-right 0.25s ease",
+        "enter-from-left": "enter-from-left 0.25s ease",
+        "exit-to-right": "exit-to-right 0.25s ease",
+        "exit-to-left": "exit-to-left 0.25s ease",
+        "scale-in-content": "scale-in-content 0.2s ease",
+        "scale-out-content": "scale-out-content 0.2s ease",
+        "fade-in": "fade-in 0.2s ease",
+        "fade-out": "fade-out 0.2s ease",
+      },
     },
   },
   plugins: [
+    require('tailwindcss-radix')({ variantPrefix: 'rdx' }),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
-    require('tailwindcss-radix')({ variantPrefix: 'rdx' }),
   ],
 }
