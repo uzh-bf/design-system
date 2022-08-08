6bdc25ac5d92ffd8ca7945ad51f576c31ebe1d68
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-08 17:35:15 +0200

    ensure dark mode is triggered by class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { TailwindColorsUZH, TailwindFonts } = require('./src/constants')
 
 module.exports = {
   content: ['src/**/**.tsx'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -11,48 +12,48 @@ module.exports = {
         ...TailwindFonts,
       },
       keyframes: {
-        "enter-from-right": {
-          "0%": { transform: "translateX(200px)", opacity: 0 },
-          "100%": { transform: "translateX(0)", opacity: 1 },
+        'enter-from-right': {
+          '0%': { transform: 'translateX(200px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
         },
-        "enter-from-left": {
-          "0%": { transform: "translateX(-200px)", opacity: 0 },
-          "100%": { transform: "translateX(0)", opacity: 1 },
+        'enter-from-left': {
+          '0%': { transform: 'translateX(-200px)', opacity: 0 },
+          '100%': { transform: 'translateX(0)', opacity: 1 },
         },
-        "exit-to-right": {
-          "0%": { transform: "translateX(0)", opacity: 1 },
-          "100%": { transform: "translateX(200px)", opacity: 0 },
+        'exit-to-right': {
+          '0%': { transform: 'translateX(0)', opacity: 1 },
+          '100%': { transform: 'translateX(200px)', opacity: 0 },
         },
-        "exit-to-left": {
-          "0%": { transform: "translateX(0)", opacity: 1 },
-          "100%": { transform: "translateX(-200px)", opacity: 0 },
+        'exit-to-left': {
+          '0%': { transform: 'translateX(0)', opacity: 1 },
+          '100%': { transform: 'translateX(-200px)', opacity: 0 },
         },
-        "scale-in-content": {
-          "0%": { transform: "rotateX(-30deg) scale(0.9)", opacity: 0 },
-          "100%": { transform: "rotateX(0deg) scale(1)", opacity: 1 },
+        'scale-in-content': {
+          '0%': { transform: 'rotateX(-30deg) scale(0.9)', opacity: 0 },
+          '100%': { transform: 'rotateX(0deg) scale(1)', opacity: 1 },
         },
-        "scale-out-content": {
-          "0%": { transform: "rotateX(0deg) scale(1)", opacity: 1 },
-          "100%": { transform: "rotateX(-10deg) scale(0.95)", opacity: 0 },
+        'scale-out-content': {
+          '0%': { transform: 'rotateX(0deg) scale(1)', opacity: 1 },
+          '100%': { transform: 'rotateX(-10deg) scale(0.95)', opacity: 0 },
         },
-        "fade-in": {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
         },
-        "fade-out": {
-          "0%": { opacity: 1 },
-          "100%": { opacity: 0 },
+        'fade-out': {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
         },
       },
       animation: {
-        "enter-from-right": "enter-from-right 0.25s ease",
-        "enter-from-left": "enter-from-left 0.25s ease",
-        "exit-to-right": "exit-to-right 0.25s ease",
-        "exit-to-left": "exit-to-left 0.25s ease",
-        "scale-in-content": "scale-in-content 0.2s ease",
-        "scale-out-content": "scale-out-content 0.2s ease",
-        "fade-in": "fade-in 0.2s ease",
-        "fade-out": "fade-out 0.2s ease",
+        'enter-from-right': 'enter-from-right 0.25s ease',
+        'enter-from-left': 'enter-from-left 0.25s ease',
+        'exit-to-right': 'exit-to-right 0.25s ease',
+        'exit-to-left': 'exit-to-left 0.25s ease',
+        'scale-in-content': 'scale-in-content 0.2s ease',
+        'scale-out-content': 'scale-out-content 0.2s ease',
+        'fade-in': 'fade-in 0.2s ease',
+        'fade-out': 'fade-out 0.2s ease',
       },
     },
   },
