4cfff3a47186264ad4268282df7d78939e5aaf74
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-08 14:38:26 +0200

    fix: disable prettier-plugin-tailwindcss for now (breaks with organize imports)

--- src/constants.js
+++ src/constants.js
@@ -60,7 +60,8 @@ const PrettierConfig = {
   singleQuote: true,
   trailingComma: 'es5',
   plugins: [
-    require('prettier-plugin-tailwindcss'),
+    // FIXME: currently cannot use tailwindcss together with organize imports (https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31)
+    // require('prettier-plugin-tailwindcss'),
     require('prettier-plugin-organize-imports'),
   ],
 }
