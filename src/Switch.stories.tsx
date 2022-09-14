88d09a07304f3b28762834a6ad1b5389d1fe6236
sjschlapbach <julius.schlapbach@gmx.net>
2022-09-14 18:11:44 +0200

    enhance: fix and enhance switch with different sizes

--- src/Switch.stories.tsx
+++ src/Switch.stories.tsx
@@ -25,3 +25,40 @@ export const Disabled = () => {
     />
   )
 }
+
+export const Sizes = () => {
+  const [isChecked1, setIsChecked1] = useState(false)
+  const [isChecked2, setIsChecked2] = useState(false)
+  const [isChecked3, setIsChecked3] = useState(false)
+
+  return (
+    <>
+      <div className="mb-3">
+        <Switch
+          id="default"
+          checked={isChecked1}
+          label="Standard switch"
+          onCheckedChange={(newValue) => setIsChecked1(newValue)}
+        />
+      </div>
+      <div className="mb-3">
+        <Switch
+          id="default"
+          checked={isChecked2}
+          size="lg"
+          label="Large switch"
+          onCheckedChange={(newValue) => setIsChecked2(newValue)}
+        />
+      </div>
+      <div>
+        <Switch
+          id="default"
+          checked={isChecked3}
+          size="sm"
+          label="Small switch"
+          onCheckedChange={(newValue) => setIsChecked3(newValue)}
+        />
+      </div>
+    </>
+  )
+}
