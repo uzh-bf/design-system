0781190cdf8d88a098327522b52c706a2cfb52f0
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-17 10:17:17 +0200

    chore: extend versionrc

--- .versionrc.js
+++ .versionrc.js
@@ -2,4 +2,16 @@ const { VersionConfigTypes } = require('./src/constants')
 
 module.exports = {
   types: VersionConfigTypes,
+  packageFiles: [
+    {
+      filename: `package.json`,
+      type: 'json',
+    },
+  ],
+  bumpFiles: [
+    {
+      filename: `package-lock.json`,
+      type: 'json',
+    },
+  ],
 }
