90abd02595bc0ba8235728f92c7c884306e3f163
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2022-10-15 22:21:34 +0200

    feat(Select): add select component (#25)
    * Add Select component
    
    * Adapt select component to take items with value und label
    
    * Add functionality to disable specific options
    
    * style: add styling to all parts
    
    * style: add small attribute
    
    * chore: remove scroll story component
    
    Co-authored-by: florinavogel <florina.vogel@gmail.com>

---
+++ src/Select.stories.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import Select from './Select'
+
+export const Default = () => {
+  const items = [
+    { value: 'appple', label: 'Apple' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'pear', label: 'Pear' },
+    { value: 'watermeldon', label: 'Watermelon' },
+    { value: 'peach', label: 'Peach' },
+    { value: 'mango', label: 'Mango' },
+  ]
+
+  return (
+    <Select
+      items={items}
+      onChange={(newValue) => {
+        console.log(newValue)
+      }}
+    />
+  )
+}
+
+export const Disabled = () => {
+  const items = [{ value: 'appple', label: 'Apple' }]
+
+  return <Select items={items} onChange={() => null} disabled />
+}
+
+export const DisabledElements = () => {
+  const items = [
+    { value: 'appple', label: 'Apple' },
+    { value: 'banana', label: 'Banana', disabled: true },
+    { value: 'pear', label: 'Pear' },
+    { value: 'watermeldon', label: 'Watermelon' },
+    { value: 'peach', label: 'Peach', disabled: true },
+    { value: 'mango', label: 'Mango' },
+  ]
+
+  return <Select items={items} onChange={() => null} />
+}
+
+export const Styled = () => {
+  const items = [
+    { value: 'appple', label: 'Apple' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'pear', label: 'Pear' },
+    { value: 'watermeldon', label: 'Watermelon' },
+    { value: 'peach', label: 'Peach' },
+    { value: 'mango', label: 'Mango' },
+  ]
+
+  return (
+    <Select
+      items={items}
+      onChange={() => null}
+      triggerStyle="bg-uzh-blue-20"
+      itemStyle="text-red-700"
+      className="bg-green-200"
+    />
+  )
+}
+
+export const Small = () => {
+  const items = [
+    { value: 'appple', label: 'Apple' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'pear', label: 'Pear' },
+    { value: 'watermeldon', label: 'Watermelon' },
+    { value: 'peach', label: 'Peach' },
+    { value: 'mango', label: 'Mango' },
+  ]
+
+  return <Select items={items} onChange={() => null} size="sm" />
+}
