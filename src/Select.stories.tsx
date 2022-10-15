efb4820742e82d10bd0e8f2849d197e525b70052
sjschlapbach <julius.schlapbach@gmx.net>
2022-10-15 21:27:12 +0200

    style: add styling to all parts

--- src/Select.stories.tsx
+++ src/Select.stories.tsx
@@ -7,17 +7,73 @@ export const Default = () => {
     { value: 'banana', label: 'Banana' },
     { value: 'pear', label: 'Pear' },
     { value: 'watermeldon', label: 'Watermelon' },
-    { value: 'peach', label: 'Peach', disabled: true },
+    { value: 'peach', label: 'Peach' },
     { value: 'mango', label: 'Mango' },
   ]
 
-  // Attention: scrolling does not work because apparently overflow is set to "hidden" on the body
   return (
     <Select
       items={items}
       onChange={(newValue) => {
         console.log(newValue)
       }}
-    ></Select>
+    />
+  )
+}
+
+export const Disabled = () => {
+  const items = [{ value: 'appple', label: 'Apple' }]
+
+  return (
+    <Select
+      items={items}
+      onChange={(newValue) => {
+        console.log(newValue)
+      }}
+      disabled
+    />
+  )
+}
+
+export const DisabledElements = () => {
+  const items = [
+    { value: 'appple', label: 'Apple' },
+    { value: 'banana', label: 'Banana', disabled: true },
+    { value: 'pear', label: 'Pear' },
+    { value: 'watermeldon', label: 'Watermelon' },
+    { value: 'peach', label: 'Peach', disabled: true },
+    { value: 'mango', label: 'Mango' },
+  ]
+
+  return (
+    <Select
+      items={items}
+      onChange={(newValue) => {
+        console.log(newValue)
+      }}
+    />
+  )
+}
+
+export const Styled = () => {
+  const items = [
+    { value: 'appple', label: 'Apple' },
+    { value: 'banana', label: 'Banana' },
+    { value: 'pear', label: 'Pear' },
+    { value: 'watermeldon', label: 'Watermelon' },
+    { value: 'peach', label: 'Peach' },
+    { value: 'mango', label: 'Mango' },
+  ]
+
+  return (
+    <Select
+      items={items}
+      onChange={(newValue) => {
+        console.log(newValue)
+      }}
+      triggerStyle="bg-uzh-blue-20"
+      itemStyle="text-red-700"
+      className="bg-green-200"
+    />
   )
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -5,31 +5,47 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as SelectPrimitive from '@radix-ui/react-select'
-import React from 'react'
+import React, { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import Button from './Button'
 
+// TODO Attention: scrolling does not work because apparently overflow is set to "hidden" on the body
+
 interface Item {
   value: string
+  disabled?: boolean // disabled React select
   label: string // displayed name
-  disabled?: boolean
 }
 
 export interface SelectProps {
   items: Item[]
+  disabled?: boolean
+  className?: string
+  triggerStyle?: string
+  itemStyle?: string
   onChange: (newValue: String) => void
 }
-export function Select({ items, onChange }: SelectProps) {
+export function Select({
+  items,
+  disabled,
+  className,
+  triggerStyle,
+  itemStyle,
+  onChange,
+}: SelectProps) {
+  const [open, setOpen] = useState(false)
+
   return (
     <SelectPrimitive.Root
       defaultValue={items[0].value}
       onValueChange={onChange}
+      onOpenChange={(open) => setOpen(open)}
     >
-      <SelectPrimitive.Trigger asChild>
-        <Button>
+      <SelectPrimitive.Trigger asChild className={triggerStyle}>
+        <Button disabled={disabled}>
           <SelectPrimitive.Value />
           <SelectPrimitive.Icon className="ml-2">
-            <FontAwesomeIcon icon={faChevronDown} />
+            <FontAwesomeIcon icon={open ? faChevronUp : faChevronDown} />
           </SelectPrimitive.Icon>
         </Button>
       </SelectPrimitive.Trigger>
@@ -37,7 +53,12 @@ export function Select({ items, onChange }: SelectProps) {
         <SelectPrimitive.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
           <FontAwesomeIcon icon={faChevronUp} />
         </SelectPrimitive.ScrollUpButton>
-        <SelectPrimitive.Viewport className="p-2 bg-white rounded-lg shadow-lg dark:bg-gray-800 z-[9999]">
+        <SelectPrimitive.Viewport
+          className={twMerge(
+            'p-1 bg-white rounded-lg shadow-lg dark:bg-gray-800 z-[9999]',
+            className
+          )}
+        >
           <SelectPrimitive.Group>
             {items.map((item, ix) => (
               <SelectPrimitive.Item
@@ -46,12 +67,11 @@ export function Select({ items, onChange }: SelectProps) {
                 value={item.value}
                 className={twMerge(
                   'relative flex items-center px-8 py-2 rounded-md text-gray-700 dark:text-gray-300 font-medium focus:bg-gray-100 dark:focus:bg-gray-900',
-                  'rdx-disabled:opacity-50',
-                  'focus:outline-none select-none'
+                  'rdx-disabled:opacity-50 focus:outline-none select-none'
                 )}
               >
                 <SelectPrimitive.ItemText>
-                  {item.label}
+                  <div className={itemStyle}>{item.label}</div>
                 </SelectPrimitive.ItemText>
                 <SelectPrimitive.ItemIndicator className="absolute inline-flex items-center left-2">
                   <FontAwesomeIcon icon={faCheck} />
