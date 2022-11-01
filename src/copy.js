40335bb3f620d39e782bd22d1005d59919f60f73
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-11-01 18:49:30 +0100

    enhance: add next13 compatible font classes with css variable

--- src/copy.js
+++ src/copy.js
@@ -7,6 +7,16 @@ fs.copyFileSync(
 )
 
 fs.copyFileSync(
+  path.resolve(__dirname, './fonts-kollektif-next13.css'),
+  path.resolve(__dirname, '../dist/fonts-kollektif-next13.css')
+)
+
+fs.copyFileSync(
   path.resolve(__dirname, './fonts-thesans.css'),
   path.resolve(__dirname, '../dist/fonts-thesans.css')
+)
+
+fs.copyFileSync(
+  path.resolve(__dirname, './fonts-thesans-next13.css'),
+  path.resolve(__dirname, '../dist/fonts-thesans-next13.css')
 )
