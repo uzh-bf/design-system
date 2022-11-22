6e23ab97270a2dd06391c1d4ea14980e25d562d5
sjschlapbach <julius.schlapbach@gmx.net>
2022-11-22 18:53:20 +0100

    chore: fix build by removing old fonts from build step

--- src/copy.js
+++ src/copy.js
@@ -10,13 +10,3 @@ fs.copyFileSync(
   path.resolve(__dirname, './fonts-kollektif-next13.css'),
   path.resolve(__dirname, '../dist/fonts-kollektif-next13.css')
 )
-
-fs.copyFileSync(
-  path.resolve(__dirname, './fonts-thesans.css'),
-  path.resolve(__dirname, '../dist/fonts-thesans.css')
-)
-
-fs.copyFileSync(
-  path.resolve(__dirname, './fonts-thesans-next13.css'),
-  path.resolve(__dirname, '../dist/fonts-thesans-next13.css')
-)
