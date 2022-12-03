bfcdb286137a2d61ae56057a85730ab05f5af837
sjschlapbach <julius.schlapbach@gmx.net>
2022-12-03 12:24:02 +0100

    refactor(Select): refactor stories code

--- src/Select.stories.tsx
+++ src/Select.stories.tsx
@@ -1,21 +1,52 @@
 import React, { useState } from 'react'
 import Select from './Select'
 
-export const Default = () => {
-  const items = [
-    { value: 'apple', label: 'Apple' },
-    { value: 'banana', label: 'Banana' },
-    { value: 'pear', label: 'Pear' },
-    { value: 'watermeldon', label: 'Watermelon' },
-    { value: 'peach', label: 'Peach' },
-    { value: 'mango', label: 'Mango' },
-  ]
+const fruits = [
+  { value: 'apple', label: 'Apple' },
+  { value: 'banana', label: 'Banana' },
+  { value: 'pear', label: 'Pear' },
+  { value: 'watermeldon', label: 'Watermelon' },
+  { value: 'peach', label: 'Peach' },
+  { value: 'mango', label: 'Mango' },
+]
+const vegetables = [
+  { value: 'carrot', label: 'Carrot' },
+  { value: 'cucumber', label: 'Cucumber' },
+  { value: 'onion', label: 'Onion' },
+  { value: 'potato', label: 'Potato' },
+  { value: 'tomato', label: 'Tomato' },
+  { value: 'broccoli', label: 'Broccoli' },
+]
+const transport = [
+  { value: 'car', label: 'Car' },
+  { value: 'bike', label: 'Bike' },
+  { value: 'train', label: 'Train' },
+  { value: 'plane', label: 'Plane' },
+  { value: 'boat', label: 'Boat' },
+  { value: 'bus', label: 'Bus' },
+]
+const programming = [
+  { value: 'javascript', label: 'JavaScript' },
+  { value: 'typescript', label: 'TypeScript' },
+  { value: 'python', label: 'Python' },
+  { value: 'ruby', label: 'Ruby' },
+  { value: 'java', label: 'Java' },
+  { value: 'csharp', label: 'C#' },
+]
 
+const groups = [
+  { items: fruits },
+  { items: vegetables, showSeparator: true },
+  { items: transport, showSeparator: true, label: 'Transport' },
+  { items: programming, showSeparator: true, label: 'Programming Languages' },
+]
+
+export const Default = () => {
   return (
     <Select
       name="default"
       placeholder="Select an item"
-      items={items}
+      items={fruits}
       onChange={(newValue) => {
         console.log(newValue)
       }}
@@ -24,46 +55,6 @@ export const Default = () => {
 }
 
 export const Groups = () => {
-  const fruits = [
-    { value: 'apple', label: 'Apple' },
-    { value: 'banana', label: 'Banana' },
-    { value: 'pear', label: 'Pear' },
-    { value: 'watermeldon', label: 'Watermelon' },
-    { value: 'peach', label: 'Peach' },
-    { value: 'mango', label: 'Mango' },
-  ]
-  const vegetables = [
-    { value: 'carrot', label: 'Carrot' },
-    { value: 'cucumber', label: 'Cucumber' },
-    { value: 'onion', label: 'Onion' },
-    { value: 'potato', label: 'Potato' },
-    { value: 'tomato', label: 'Tomato' },
-    { value: 'broccoli', label: 'Broccoli' },
-  ]
-  const transport = [
-    { value: 'car', label: 'Car' },
-    { value: 'bike', label: 'Bike' },
-    { value: 'train', label: 'Train' },
-    { value: 'plane', label: 'Plane' },
-    { value: 'boat', label: 'Boat' },
-    { value: 'bus', label: 'Bus' },
-  ]
-  const programming = [
-    { value: 'javascript', label: 'JavaScript' },
-    { value: 'typescript', label: 'TypeScript' },
-    { value: 'python', label: 'Python' },
-    { value: 'ruby', label: 'Ruby' },
-    { value: 'java', label: 'Java' },
-    { value: 'csharp', label: 'C#' },
-  ]
-
-  const groups = [
-    { items: fruits },
-    { items: vegetables, showSeparator: true },
-    { items: transport, showSeparator: true, label: 'Transport' },
-    { items: programming, showSeparator: true, label: 'Programming Languages' },
-  ]
-
   return (
     <Select
       name="default"
@@ -111,21 +102,12 @@ export const DisabledElements = () => {
 }
 
 export const Styled = () => {
-  const items = [
-    { value: 'apple', label: 'Apple' },
-    { value: 'banana', label: 'Banana' },
-    { value: 'pear', label: 'Pear' },
-    { value: 'watermeldon', label: 'Watermelon' },
-    { value: 'peach', label: 'Peach' },
-    { value: 'mango', label: 'Mango' },
-  ]
-
   return (
     <div className="w-full h-12 border border-solid rounded-md">
       <Select
         name="styled_select"
         placeholder="Select an item"
-        items={items}
+        items={fruits}
         onChange={() => null}
         className={{
           root: 'h-full border-none rounded-none',
@@ -139,20 +121,11 @@ export const Styled = () => {
 }
 
 export const Small = () => {
-  const items = [
-    { value: 'apple', label: 'Apple' },
-    { value: 'banana', label: 'Banana' },
-    { value: 'pear', label: 'Pear' },
-    { value: 'watermeldon', label: 'Watermelon' },
-    { value: 'peach', label: 'Peach' },
-    { value: 'mango', label: 'Mango' },
-  ]
-
   return (
     <Select
       name="small_select"
       placeholder="Select an item"
-      items={items}
+      items={fruits}
       onChange={() => null}
       size="sm"
     />
