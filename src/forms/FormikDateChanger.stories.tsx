ba28512731ae6e71216a4148d2a5eaf1624310a7
sjschlapbach <julius.schlapbach@gmx.net>
2023-02-18 14:39:02 +0100

    wip: add formik date changer component

---
+++ src/forms/FormikDateChanger.stories.tsx
@@ -0,0 +1,30 @@
+import { Form, Formik } from 'formik'
+import React from 'react'
+import Button from '../Button'
+import FormikDateChanger from './FormikDateChanger'
+
+export const Default = () => (
+  <div>
+    <Formik
+      initialValues={{
+        date: '2023-01-01',
+      }}
+      onSubmit={async (values, { resetForm }) => {
+        alert(`Form submitted with date: ${values.date}`)
+        resetForm()
+      }}
+    >
+      {({ values }) => {
+        return (
+          <div>
+            <Form>
+              <FormikDateChanger name="date" className={{ root: 'mb-2' }} />
+              <Button type="submit">Submit</Button>
+            </Form>
+            <div>Value: {values.date}</div>
+          </div>
+        )
+      }}
+    </Formik>
+  </div>
+)
