ff13511bb4f82105c849981b54154f0a88b8ac66
sjschlapbach <julius.schlapbach@gmx.net>
2023-02-18 13:45:35 +0100

    chore(TextField): add docstring to text field

--- src/forms/TextField.tsx
+++ src/forms/TextField.tsx
@@ -30,6 +30,26 @@ export interface TextFieldProps {
   onChange: (newValue: string) => void
 }
 
+/**
+ * This function returns a text field component for use without formik
+ *
+ * @param id - The id of the input field.
+ * @param data - The object of data attributes that can be used for testing (e.g. data-test or data-cy)
+ * @param label - The text displayed as label.
+ * @param placeholder - The placeholder text for the input field.
+ * @param tooltip - The optional tooltip is shown on hover over the tooltip next to the label.
+ * @param required - Indicate whether the field is required or not.
+ * @param hideError - Indicate whether the error message should be hidden or not.
+ * @param hasError - Indicate whether the field has an error or not (validation is not handled by this component).
+ * @param isTouched - Indicate whether the field has been touched or not (validation is not handled by this component).
+ * @param disabled - Indicate whether the field is disabled or not.
+ * @param className - The optional className object allows you to override the default styling.
+ * @param icon - The optional icon is shown on the right side of the input field.
+ * @param value - The value of the input field (external state management).
+ * @param onChange - The onChange function of the input field (external state management).
+ * @returns Text field component with optional label, tooltip, error message and icon.
+ */
+
 export function TextField({
   id,
   data,
