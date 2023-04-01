f43f7c30c161ef1dff3a233610abf044dd5f9085
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2023-04-01 10:05:59 +0200

    feat(StepProgress): add stepwise progress component (#52)
    * feat(StepProgress): add stepwise progress component
    
    * refactor(Progress): remove replaced nonlinear component to simplify structure
    
    * chore(StepProgress): enforce definition of onClick method
    
    * chore(Table): fix user notification usage

--- src/Progress.tsx
+++ src/Progress.tsx
@@ -1,11 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-import {
-  faChevronLeft,
-  faChevronRight,
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as RadixProgress from '@radix-ui/react-progress'
 import { ThemeContext } from './ThemeProvider'
 
@@ -20,23 +15,11 @@ export interface ProgressProps {
   max: number
   formatter: (value: any) => any
   isMaxVisible?: boolean
-  nonLinear?: boolean
-  displayOffset?: number
-  onItemClick?: (ix: number) => void
   className?: {
     root?: string
     indicator?: string
   }
   [x: string]: any
-}
-
-export interface ContinuousProgressProps extends ProgressProps {
-  offset?: number
-  nonLinear?: false
-}
-export interface NonLinearProgressProps extends ProgressProps {
-  offset?: never
-  nonLinear: true
 }
 
 /**
@@ -49,9 +32,6 @@ export interface NonLinearProgressProps extends ProgressProps {
  * @param max - The maximum value of the progress bar.
  * @param formatter - The function that formats the value of the progress bar.
  * @param isMaxVisible - The boolean that determines if the maximum value should be displayed.
- * @param nonLinear - The boolean that determines if the progress bar should have discrete steps and allow the user to jump between different elements.
- * @param displayOffset - The number that determines the maximum number of elements that are shown on a non-linear progress bar.
- * @param onItemClick - The function that is called when a non-linear progress bar item is clicked.
  * @param className - The optional className object allows you to override the default styling.
  * @return Progress component
  */
@@ -64,11 +44,8 @@ export function Progress({
   max,
   className,
   isMaxVisible = true,
-  nonLinear = false,
-  displayOffset,
-  onItemClick,
   ...props
-}: ContinuousProgressProps | NonLinearProgressProps) {
+}: ProgressProps) {
   const theme = useContext(ThemeContext)
   const [internalValue, setInternalValue] = useState(0)
 
@@ -91,7 +68,7 @@ export function Progress({
       {...props}
     >
       <div className="absolute flex flex-col justify-center w-full h-full px-2 py-1 text-right bg-gray-200 rounded">
-        {!nonLinear && isMaxVisible && formatter(max)}
+        {isMaxVisible && formatter(max)}
       </div>
 
       <RadixProgress.Indicator
@@ -100,60 +77,12 @@ export function Progress({
         }}
         className={twMerge(
           'absolute px-2 py-1 min-w-[40px] h-full text-white rounded flex flex-col justify-center text-right',
-          !nonLinear && theme.primaryBgMedium,
+          theme.primaryBgMedium,
           className?.indicator
         )}
       >
-        {!nonLinear && formatter(value)}
+        {formatter(value)}
       </RadixProgress.Indicator>
-
-      {nonLinear && (
-        <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-row text-xs">
-          {displayOffset && value - displayOffset > 0 && (
-            <button
-              className={twMerge(
-                'px-3 py-1 rounded-l text-white',
-                theme.primaryBgMedium,
-                theme.primaryTextHover,
-                theme.primaryBgHover
-              )}
-              onClick={() => onItemClick && onItemClick(value - 1)}
-            >
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </button>
-          )}
-          {new Array(max).fill(0).map((item, ix) => (
-            <button
-              className={twMerge(
-                'p-1 flex-1',
-                ix === 0 && 'rounded-l',
-                ix === max - 1 && 'rounded-r',
-                value > ix && `text-white ${theme.primaryBgMedium}`,
-                value === ix && `font-bold bg-gray-400 text-white`,
-                displayOffset && ix < value - displayOffset && 'hidden',
-                displayOffset && ix > value + displayOffset && 'hidden',
-                theme.primaryTextHover,
-                theme.primaryBgHover
-              )}
-              onClick={() => onItemClick && onItemClick(ix)}
-            >
-              {ix + 1}
-            </button>
-          ))}
-          {displayOffset && max > value + displayOffset + 1 && (
-            <button
-              className={twMerge(
-                'px-3 py-1 rounded-r',
-                theme.primaryTextHover,
-                theme.primaryBgHover
-              )}
-              onClick={() => onItemClick && onItemClick(value + 1)}
-            >
-              <FontAwesomeIcon icon={faChevronRight} />
-            </button>
-          )}
-        </div>
-      )}
     </RadixProgress.Root>
   )
 }
