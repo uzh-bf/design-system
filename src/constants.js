ade237e544802fe752479ddda5838fe51c9a297c
sjschlapbach <julius.schlapbach@gmx.net>
2023-06-15 23:03:58 +0200

    enhance(ColorPicker): add color picker component

--- src/constants.js
+++ src/constants.js
@@ -173,6 +173,14 @@ const VersionConfigTypes = [
   },
 ]
 
+const PRESET_COURSE_COLORS = [
+  '#262FAD',
+  '#016272',
+  '#5FB1F9',
+  '#FE7408',
+  '#D84B39',
+]
+
 module.exports = {
   TailwindAnimations,
   TailwindColorsUZH,
@@ -182,4 +190,5 @@ module.exports = {
   ESLintConfig,
   PrettierConfig,
   VersionConfigTypes,
+  PRESET_COURSE_COLORS,
 }
