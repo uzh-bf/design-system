481c44a254c89ea5b5ab4ca039d2be447d912a06
sjschlapbach <julius.schlapbach@gmx.net>
2023-06-16 10:46:17 +0200

    feat(FormikColorPicker): add formik color picker component

---
+++ src/forms/FormikColorPicker.stories.tsx
@@ -0,0 +1,96 @@
+import { Form, Formik } from 'formik'
+import React from 'react'
+import Button from '../Button'
+import FormikColorPicker from './FormikColorPicker'
+
+export const Default = () => (
+  <div>
+    <Formik
+      initialValues={{
+        color: '#FF0000',
+      }}
+      onSubmit={async (values, { resetForm }) => {
+        alert(
+          `Form submitted with color: ${values.color}. The form will be reset.`
+        )
+        resetForm()
+      }}
+    >
+      {({ values }) => {
+        return (
+          <div>
+            <Form>
+              <FormikColorPicker name="color" className={{ root: 'mb-4' }} />
+              <Button type="submit">Submit</Button>
+            </Form>
+            <div>Value: {values.color}</div>
+          </div>
+        )
+      }}
+    </Formik>
+  </div>
+)
+
+export const Disabled = () => (
+  <div>
+    <Formik
+      initialValues={{
+        color: '#FF0000',
+      }}
+      onSubmit={async (values, { resetForm }) => {
+        alert(
+          `Form submitted with color: ${values.color}. The form will be reset.`
+        )
+        resetForm()
+      }}
+    >
+      {({ values }) => {
+        return (
+          <div>
+            <Form>
+              <FormikColorPicker
+                disabled
+                name="color"
+                className={{ root: 'mb-4' }}
+              />
+              <Button type="submit">Submit</Button>
+            </Form>
+            <div>Value: {values.color}</div>
+          </div>
+        )
+      }}
+    </Formik>
+  </div>
+)
+
+export const CustomPresets = () => (
+  <div>
+    <Formik
+      initialValues={{
+        color: '#FF0000',
+      }}
+      onSubmit={async (values, { resetForm }) => {
+        alert(
+          `Form submitted with color: ${values.color}. The form will be reset.`
+        )
+        resetForm()
+      }}
+    >
+      {({ values }) => {
+        return (
+          <div>
+            <Form>
+              <FormikColorPicker
+                name="color"
+                className={{ root: 'mb-4' }}
+                presetColors={['#FF0000', '#00FF00', '#0000FF']}
+              />
+              <Button type="submit">Submit</Button>
+            </Form>
+            <div>Value: {values.color}</div>
+          </div>
+        )
+      }}
+    </Formik>
+  </div>
+)
