8957b28a865f9bdf8450183a3734cb3677b6711d
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2023-07-18 18:05:50 +0200

    enhance(StepProgress): add status and highlighting to step progress elements (#62)

--- src/StepProgress.stories.tsx
+++ src/StepProgress.stories.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import StepProgress from './StepProgress'
+import React, { useState } from 'react'
+import StepProgress, { StepItem } from './StepProgress'
 
 export const Default = () => {
-  const [value, setValue] = React.useState(5)
+  const [value, setValue] = useState(5)
 
   return (
     <StepProgress value={value} onItemClick={(val) => setValue(val)} max={20} />
@@ -18,6 +18,60 @@ export const WithOffset = () => {
       max={20}
       displayOffset={7}
       onItemClick={(val) => setValue(val)}
+    />
+  )
+}
+
+const statusItems: StepItem[] = [
+  { status: 'correct', id: 'id_1' },
+  { status: 'incorrect', id: 'id_2' },
+  {
+    status: 'unanswered',
+    id: 'id_3',
+  },
+  { status: 'partial', id: 'id_4' },
+  { status: 'correct', id: 'id_5' },
+  { status: 'correct', id: 'id_6' },
+  {
+    status: 'unanswered',
+    id: 'id_7',
+  },
+  {
+    status: 'unanswered',
+    id: 'id_8',
+  },
+  {
+    status: 'unanswered',
+    id: 'id_9',
+  },
+]
+
+export const Status = () => {
+  const [value, setValue] = useState(6)
+
+  return (
+    <StepProgress
+      value={value}
+      onItemClick={(val, item) => {
+        alert('Element that is being activated has id: ' + item?.id)
+        setValue(val)
+      }}
+      items={statusItems}
+    />
+  )
+}
+
+export const StatusOffset = () => {
+  const [value, setValue] = useState(6)
+
+  return (
+    <StepProgress
+      value={value}
+      onItemClick={(val, _) => {
+        setValue(val)
+      }}
+      items={statusItems}
+      displayOffset={2}
     />
   )
 }
--- src/StepProgress.tsx
+++ src/StepProgress.tsx
@@ -1,24 +1,41 @@
 import {
+  faCheck,
+  faCheckDouble,
   faChevronLeft,
   faChevronRight,
+  faX,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
-export interface StepProgressProps {
+export interface StepItem {
+  status: 'correct' | 'incorrect' | 'partial' | 'unanswered'
+  [x: string]: any
+}
+
+interface StepProgressBaseProps {
   id?: string
   data?: {
     cy?: string
     test?: string
   }
   value: number
-  max: number
-  onItemClick: (ix: number) => void
+  onItemClick: (ix: number, item?: StepItem) => void
   displayOffset?: number
   className?: {
     root?: string
   }
+}
+
+export interface StepProgressProps extends StepProgressBaseProps {
+  max: number
+  items?: never
+}
+
+export interface StepProgressItemProps extends StepProgressBaseProps {
+  max?: never
+  items: StepItem[]
 }
 
 /**
@@ -38,10 +55,14 @@ export function StepProgress({
   data,
   value,
   max,
+  items,
   onItemClick,
   displayOffset,
   className,
-}: StepProgressProps) {
+}: StepProgressProps | StepProgressItemProps) {
+  const length = items ? items.length : max
+  const elements = items || new Array(length).fill(0)
+
   return (
     <div
       className={twMerge(
@@ -55,35 +76,48 @@ export function StepProgress({
       {displayOffset && value - displayOffset > 0 && (
         <button
           className={twMerge(
-            'rounded-l bg-primary-60 px-3 py-1 text-white hover:bg-primary-20 hover:text-primary'
+            'rounded-l px-3 py-1 hover:bg-primary-20 hover:text-primary',
+            !items && 'bg-primary-60 text-white'
           )}
-          onClick={() => onItemClick(value - 1)}
+          onClick={() => onItemClick(value - 1, items && items[value - 1])}
         >
           <FontAwesomeIcon icon={faChevronLeft} />
         </button>
       )}
-      {new Array(max).fill(0).map((_, ix) => (
+      {elements.map((element, ix) => (
         <button
           className={twMerge(
-            'flex-1 p-1 hover:bg-primary-20 hover:text-primary',
+            'flex flex-1 items-center justify-center p-1 hover:bg-primary-20 hover:text-primary',
             ix === 0 && 'rounded-l',
-            ix === max - 1 && 'rounded-r',
-            value > ix && `bg-primary-60 text-white`,
-            value === ix && `bg-gray-400 font-bold text-white`,
+            ix === length - 1 && 'rounded-r',
+            value > ix && !items && 'bg-primary-60 text-white',
+            value === ix && 'bg-gray-400 font-bold text-white',
+            element.status === 'correct' &&
+              'bg-green-600 bg-opacity-60 text-white',
+            element.status === 'incorrect' &&
+              'bg-red-600 bg-opacity-60 text-white',
+            element.status === 'partial' &&
+              'bg-uzh-red-100 bg-opacity-60 text-white',
+            value === ix && 'bg-opacity-100',
             displayOffset && ix < value - displayOffset && 'hidden',
             displayOffset && ix > value + displayOffset && 'hidden'
           )}
-          onClick={() => onItemClick(ix)}
+          onClick={() => onItemClick(ix, items && items[ix])}
         >
-          {ix + 1}
+          {element.status === 'correct' && (
+            <FontAwesomeIcon icon={faCheckDouble} />
+          )}
+          {element.status === 'incorrect' && <FontAwesomeIcon icon={faX} />}
+          {element.status === 'partial' && <FontAwesomeIcon icon={faCheck} />}
+          {(!element.status || element.status === 'unanswered') && (
+            <div>{ix + 1}</div>
+          )}
         </button>
       ))}
-      {displayOffset && max > value + displayOffset + 1 && (
+      {displayOffset && length > value + displayOffset + 1 && (
         <button
-          className={twMerge(
-            'rounded-r px-3 py-1 hover:bg-primary-20 hover:text-primary'
-          )}
-          onClick={() => onItemClick(value + 1)}
+          className="rounded-r px-3 py-1 hover:bg-primary-20 hover:text-primary"
+          onClick={() => onItemClick(value + 1, items && items[value + 1])}
         >
           <FontAwesomeIcon icon={faChevronRight} />
         </button>
