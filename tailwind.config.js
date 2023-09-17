7deb8ac93054bd30d580d5881384897e59a69e1a
Roland Schläfli <rolandschlaefli@gmail.com>
2023-09-17 17:47:46 +0200

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,77 @@
-const {
-  TailwindColorsUZH,
-  TailwindFonts,
-  TailwindAnimations,
-} = require('./src/constants')
+const { TailwindColorsUZH } = require('./src/constants')
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ['src/**/**.tsx'],
   theme: {
     extend: {
-      ...TailwindAnimations,
       colors: {
         ...TailwindColorsUZH,
-        primary: 'var(--theme-color-primary)',
-        'primary-80': 'var(--theme-color-primary-80)',
-        'primary-60': 'var(--theme-color-primary-60)',
-        'primary-40': 'var(--theme-color-primary-40)',
-        'primary-20': 'var(--theme-color-primary-20)',
-        secondary: 'var(--theme-color-secondary)',
-        'secondary-80': 'var(--theme-color-secondary-80)',
-        'secondary-60': 'var(--theme-color-secondary-60)',
-        'secondary-40': 'var(--theme-color-secondary-40)',
-        'secondary-20': 'var(--theme-color-secondary-20)',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+          80: 'hsl(var(--primary-80))',
+          60: 'hsl(var(--primary-60))',
+          40: 'hsl(var(--primary-40))',
+          20: 'hsl(var(--primary-20))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+          80: 'hsl(var(--secondary-80))',
+          60: 'hsl(var(--secondary-60))',
+          40: 'hsl(var(--secondary-40))',
+          20: 'hsl(var(--secondary-20))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: 'calc(var(--radius) - 4px)',
       },
       fontFamily: {
-        ...TailwindFonts,
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
@@ -32,6 +80,7 @@ module.exports = {
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('tailwindcss-animate'),
   ],
   corePlugins: {
     preflight: false,
