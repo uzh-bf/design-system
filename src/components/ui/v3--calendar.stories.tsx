00afa1417b4f627ae03f92c44ecb58a76787f2b6
Roland Schläfli <rolandschlaefli@gmail.com>
2023-09-17 20:12:55 +0200

    datepicker and calendar

---
+++ src/components/ui/v3--calendar.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Calendar } from './calendar'
+
+export const Default = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+  return (
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+    />
+  )
+}
