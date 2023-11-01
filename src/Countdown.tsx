daa67aae659c0a432c5ac6d4168d4ba679e0cd4c
sjschlapbach <julius.schlapbach@gmx.net>
2023-11-01 16:19:43 +0100

    chore(Countdown): add prop descriptions for countdown component

--- src/Countdown.tsx
+++ src/Countdown.tsx
@@ -16,6 +16,18 @@ interface CountdownProps {
   }
 }
 
+/**
+ * This function creates a simple text countdown component (without any additional features or styling)
+ *
+ * @param isStatic - If true, the countdown will not be running, but instead show the initial value. However, as the end value is given by a date, reloading can modify the displayed countdown value
+ * @param expiresAt - Date when the countdown should expire
+ * @param formatter - Function to format the countdown value
+ * @param onExpire - Function that is executed when the countdown expires
+ * @param onUpdate - Function that is executed when the countdown is updated (not when it expires)
+ * @param data - Optional data object that can be used for testing (e.g. data-test or data-cy)
+ * @param className - Optional className object allows you to override the default styling
+ * @returns A simple text countdown component
+ */
 function Countdown({
   isStatic,
   expiresAt,
