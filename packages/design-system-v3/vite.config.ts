89ed7fab4d7df2b9eb11d38267b587be62f38687
Roland Schlaefli <rolandschlaefli@gmail.com>
2024-01-21 15:22:07 +0100

    working build with vite

--- packages/design-system-v3/vite.config.ts
+++ packages/design-system-v3/vite.config.ts
@@ -5,6 +5,23 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      // output: {
+      //   // Provide global variables to use in the UMD build
+      //   // for externalized deps
+      //   globals: {
+      //     vue: 'Vue',
+      //   },
+      // },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
