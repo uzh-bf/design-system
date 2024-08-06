31fa9b1d0f32eeee1a7267146733f42e2c7b8e5d
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-06 23:09:46 +0200

    start vite migration

---
+++ packages/design-system-vite/src/Button.tsx
@@ -0,0 +1,3 @@
+export default function Button({ children }: { children: React.ReactNode }) {
+  return <button>{children}</button>
+}
---
+++ packages/design-system-vite/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ rollupTypes: true })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
+  },
+})
