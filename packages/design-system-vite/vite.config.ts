57edc126264b60806c8a589c69c64f148b977cae
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-07 08:56:38 +0200

    add prettier

--- packages/design-system-vite/vite.config.ts
+++ packages/design-system-vite/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
