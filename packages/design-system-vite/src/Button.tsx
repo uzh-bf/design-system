2479bafc4e3b67d6ddd1f54f2ec4e0e066aaff36
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-07 10:25:48 +0200

    ts for ladle

--- packages/design-system-vite/src/Button.tsx
+++ packages/design-system-vite/src/Button.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react'
+
 export default function Button({ children }: { children: React.ReactNode }) {
   return <button className="bg-blue-500 p-4 text-white">{children}</button>
 }
