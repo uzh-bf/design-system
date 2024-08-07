3c2d0082a65edae36166c764dd19911aa1c04481
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-07 09:40:34 +0200

    add ladle

---
+++ packages/design-system-vite/src/Button.stories.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+import type { Story } from '@ladle/react'
+
+import Button from './Button.js'
+
+export const ButtonStory: Story = () => <Button>My Button</Button>
--- packages/design-system-vite/vite.config.ts
+++ packages/design-system-vite/vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
