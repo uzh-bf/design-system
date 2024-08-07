9a5b482c1589104b882ae42b56dd792ede1170c7
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-07 11:14:39 +0200

    add checkbox

--- packages/design-system-vite/src/index.ts
+++ packages/design-system-vite/src/index.ts
@@ -1,3 +1,4 @@
 import './tailwind.css'
 
 export { default as Button } from './Button.js'
+export { default as Checkbox } from './Checkbox.js'
--- packages/design-system-vite/vite.config.ts
+++ packages/design-system-vite/vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
