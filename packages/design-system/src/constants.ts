2234686a8db958c32bc358ec69d9f6005f9360c8
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-15 17:51:38 +0200

    fix(design-system): add primary and secondary colors and shadcn colors to constants file

--- packages/design-system/src/constants.ts
+++ packages/design-system/src/constants.ts
@@ -83,6 +83,49 @@ export const TailwindColorsUZH = {
   'uzh-turqoise-60': '#6bb7c7',
   'uzh-turqoise-40': '#9ed0d9',
   'uzh-turqoise-20': '#cfe8ec',
+  'primary-100': 'var(--theme-color-primary)',
+  'primary-80': 'var(--theme-color-primary-80)',
+  'primary-60': 'var(--theme-color-primary-60)',
+  'primary-40': 'var(--theme-color-primary-40)',
+  'primary-20': 'var(--theme-color-primary-20)',
+  'secondary-100': 'var(--theme-color-secondary)',
+  'secondary-80': 'var(--theme-color-secondary-80)',
+  'secondary-60': 'var(--theme-color-secondary-60)',
+  'secondary-40': 'var(--theme-color-secondary-40)',
+  'secondary-20': 'var(--theme-color-secondary-20)',
+  border: 'hsl(var(--border))',
+  input: 'hsl(var(--input))',
+  ring: 'hsl(var(--ring))',
+  background: 'hsl(var(--background))',
+  foreground: 'hsl(var(--foreground))',
+  primary: {
+    DEFAULT: 'hsl(var(--primary))',
+    foreground: 'hsl(var(--primary-foreground))',
+  },
+  secondary: {
+    DEFAULT: 'hsl(var(--secondary))',
+    foreground: 'hsl(var(--secondary-foreground))',
+  },
+  destructive: {
+    DEFAULT: 'hsl(var(--destructive))',
+    foreground: 'hsl(var(--destructive-foreground))',
+  },
+  muted: {
+    DEFAULT: 'hsl(var(--muted))',
+    foreground: 'hsl(var(--muted-foreground))',
+  },
+  accent: {
+    DEFAULT: 'hsl(var(--accent))',
+    foreground: 'hsl(var(--accent-foreground))',
+  },
+  popover: {
+    DEFAULT: 'hsl(var(--popover))',
+    foreground: 'hsl(var(--popover-foreground))',
+  },
+  card: {
+    DEFAULT: 'hsl(var(--card))',
+    foreground: 'hsl(var(--card-foreground))',
+  },
 }
 
 export const TailwindFonts = {
--- packages/design-system/tailwind.config.js
+++ packages/design-system/tailwind.config.js
@@ -23,55 +23,8 @@ export default {
     },
     extend: {
       ...TailwindAnimations,
-      fontFamily: {
-        ...TailwindFonts,
-      },
-      colors: {
-        ...TailwindColorsUZH,
-        'primary-100': 'var(--theme-color-primary)',
-        'primary-80': 'var(--theme-color-primary-80)',
-        'primary-60': 'var(--theme-color-primary-60)',
-        'primary-40': 'var(--theme-color-primary-40)',
-        'primary-20': 'var(--theme-color-primary-20)',
-        'secondary-100': 'var(--theme-color-secondary)',
-        'secondary-80': 'var(--theme-color-secondary-80)',
-        'secondary-60': 'var(--theme-color-secondary-60)',
-        'secondary-40': 'var(--theme-color-secondary-40)',
-        'secondary-20': 'var(--theme-color-secondary-20)',
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-      },
+      fontFamily: TailwindFonts,
+      colors: TailwindColorsUZH,
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
