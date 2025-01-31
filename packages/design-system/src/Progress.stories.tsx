ae8e0aa9d9fb2258d47f18b1636990d4994a5354
sjschlapbach <julius.schlapbach@gmx.net>
2025-01-31 21:53:36 +0100

    enhance(Progress): extend progress component to support stacked progress bars

--- packages/design-system/src/Progress.stories.tsx
+++ packages/design-system/src/Progress.stories.tsx
@@ -30,3 +30,55 @@ export const Styled = () => {
 export const Offset = () => {
   return <Progress offset={50} value={60} max={100} formatter={(val) => val} />
 }
+
+export const Multiple = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <Progress
+        value={[30, 70]}
+        max={100}
+        formatter={(val) => val}
+        className={{ indicator: ['bg-blue-400', 'bg-red-400'] }}
+      />
+      <Progress
+        value={[70, 20]}
+        max={100}
+        formatter={() => null}
+        className={{
+          background: 'bg-gray-300',
+          indicator: [
+            'rounded-r-none bg-green-600',
+            'rounded-r-none bg-gray-300',
+          ],
+        }}
+      />
+      <Progress
+        noMinWidth
+        value={[70, 0]}
+        max={100}
+        formatter={() => null}
+        className={{
+          background: 'bg-gray-300',
+          indicator: [
+            'min-w-0 rounded-r-none bg-green-600',
+            'min-w-0 rounded-r-none bg-gray-300',
+          ],
+        }}
+      />
+      <Progress
+        value={[85, 60, 30, 10]}
+        max={100}
+        formatter={() => null}
+        className={{
+          background: 'bg-gray-300',
+          indicator: [
+            'rounded-r-none bg-green-600',
+            'rounded-r-none bg-orange-300',
+            'rounded-r-none bg-blue-600',
+            'rounded-r-none bg-red-600',
+          ],
+        }}
+      />
+    </div>
+  )
+}
--- packages/design-system/src/Progress.tsx
+++ packages/design-system/src/Progress.tsx
@@ -1,37 +1,53 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 import * as RadixProgress from '@radix-ui/react-progress'
 
-export interface ProgressProps {
+interface BaseProgressProps {
   id?: string
   data?: {
     cy?: string
     test?: string
   }
-  value: number
   offset?: number
   max: number
-  formatter: (value: any) => any
+  formatter: (value: string | number) => string | React.ReactNode
   isMaxVisible?: boolean
+  noMinWidth?: boolean
+  [x: string]: unknown
+}
+
+interface SingleValueProgressProps extends BaseProgressProps {
+  value: number
   className?: {
     root?: string
     background?: string
     indicator?: string
   }
-  [x: string]: any
 }
+
+interface MultiValueProgressProps extends BaseProgressProps {
+  value: number[]
+  className?: {
+    root?: string
+    background?: string
+    indicator?: string[]
+  }
+}
+
+export type ProgressProps = SingleValueProgressProps | MultiValueProgressProps
 
 /**
  * This function returns a pre-styled Progress component based on the RadixUI progress component and the custom theme.
  *
  * @param id - The id of the progress bar.
  * @param data - The object of data attributes that can be used for testing (e.g. data-test or data-cy)
- * @param value - The value of the progress bar. The value should be between 0 and an optionally provided max value.
+ * @param value - The value of the progress bar. The value should be between 0 and an optionally provided max value. If multiple values are provided, the indicators are rendered on top of each other (allowing for stacked progress bars).
  * @param offset - The number that determines the offset of the progress bar. The offset is subtracted from the value.
  * @param max - The maximum value of the progress bar.
  * @param formatter - The function that formats the value of the progress bar.
  * @param isMaxVisible - The boolean that determines if the maximum value should be displayed.
+ * @param noMinWidth - The boolean that determines if the progress bar should have a minimum width.
  * @param className - The optional className object allows you to override the default styling.
  * @return Progress component
  */
@@ -44,25 +60,16 @@ export function Progress({
   max,
   className,
   isMaxVisible = true,
+  noMinWidth = false,
   ...props
 }: ProgressProps) {
-  const [internalValue, setInternalValue] = useState(0)
-
-  useEffect(() => {
-    if (offset > 0) {
-      setInternalValue(Math.max(value - offset, 0))
-    } else {
-      setInternalValue(value)
-    }
-  }, [value, offset])
-
   return (
     <RadixProgress.Root
       id={id}
       data-cy={data?.cy}
       data-test={data?.test}
       className={twMerge('relative h-7 rounded text-sm', className?.root)}
-      value={internalValue}
+      value={typeof value === 'number' ? value : value[0]}
       max={max}
       {...props}
     >
@@ -75,18 +82,69 @@ export function Progress({
         {isMaxVisible && formatter(max)}
       </div>
 
-      <RadixProgress.Indicator
-        style={{
-          width: `${(internalValue / Math.max(max - offset, 0)) * 100}%`,
-        }}
-        className={twMerge(
-          'absolute flex h-full min-w-[40px] flex-col justify-center rounded bg-primary-60 px-2 py-1 text-right text-white',
-          className?.indicator
-        )}
-      >
-        {formatter(value)}
-      </RadixProgress.Indicator>
+      {typeof value === 'number' ? (
+        <ProgressIndicator
+          value={value}
+          offset={offset}
+          max={max}
+          formatter={formatter}
+          noMinWidth={noMinWidth}
+          className={className?.indicator as string | undefined}
+        />
+      ) : (
+        value.map((v, ix) => (
+          <ProgressIndicator
+            value={v}
+            offset={offset}
+            max={max}
+            formatter={formatter}
+            noMinWidth={noMinWidth}
+            className={className?.indicator?.[ix]}
+          />
+        ))
+      )}
     </RadixProgress.Root>
+  )
+}
+
+function ProgressIndicator({
+  value,
+  offset,
+  max,
+  formatter,
+  noMinWidth,
+  className,
+}: {
+  value: number
+  offset: number
+  max: number
+  formatter: (value: string | number) => string | React.ReactNode
+  noMinWidth: boolean
+  className?: string
+}) {
+  const [internalValue, setInternalValue] = useState(0)
+
+  useEffect(() => {
+    if (offset > 0) {
+      setInternalValue(Math.max(value - offset, 0))
+    } else {
+      setInternalValue(value)
+    }
+  }, [value, offset])
+
+  return (
+    <RadixProgress.Indicator
+      style={{
+        width: `${(internalValue / Math.max(max - offset, 0)) * 100}%`,
+      }}
+      className={twMerge(
+        'absolute flex h-full flex-col justify-center rounded bg-primary-60 py-1 text-right text-white',
+        !noMinWidth && 'min-w-[40px] px-2',
+        className
+      )}
+    >
+      {formatter(value)}
+    </RadixProgress.Indicator>
   )
 }
 
