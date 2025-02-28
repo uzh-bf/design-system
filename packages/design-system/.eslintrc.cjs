5d8e5008089a618a898a29f6eff1afff623b1ffc
sjschlapbach <julius.schlapbach@gmx.net>
2025-02-28 15:47:16 +0100

    chore: remove unused comment

--- packages/design-system/.eslintrc.cjs
+++ packages/design-system/.eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'ui', 'types'], // Added 'types' to ignore patterns
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'ui', 'types'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
