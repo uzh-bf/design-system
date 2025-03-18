15f4a0add712cde29c8c6c17611c118fae3ef2f8
sjschlapbach <julius.schlapbach@gmx.net>
2025-03-18 10:42:36 +0100

    chore: fix wrong svg attribute in button loading icon

--- packages/design-system/src/Button.tsx
+++ packages/design-system/src/Button.tsx
@@ -106,7 +106,7 @@ export function Button({
             cy="12"
             r="10"
             stroke="currentColor"
-            stroke-width="4"
+            strokeWidth="4"
           ></circle>
           <path
             className="opacity-75"
