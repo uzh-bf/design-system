668288e3544777774b96bfd215913818fa6d432c
sjschlapbach <julius.schlapbach@gmx.net>
2025-05-27 21:45:31 +0200

    chore: add controlled state as story for tabs

--- packages/design-system/src/Tabs.stories.tsx
+++ packages/design-system/src/Tabs.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Tabs from './Tabs'
 
 export const Default = () => {
@@ -27,37 +28,32 @@ export const Default = () => {
   )
 }
 
-// TODO: re-introduce when fixed
-// export const Controlled = () => {
-//   const [selectedTab, setSelectedTab] = useState('leaderboard')
-//   return (
-//     <Tabs
-//       defaultValue="leaderboard"
-//       value={selectedTab}
-//       onValueChange={(tab) => setSelectedTab(tab)}
-//     >
-//       <Tabs.TabList>
-//         <Tabs.Tab key="leaderboard" value="leaderboard" label="Leaderboard" />
-//         <Tabs.Tab
-//           key="create"
-//           value="create"
-//           label="Gruppe erstellen/beitreten"
-//         />
-//       </Tabs.TabList>
-//       <Tabs.TabContent
-//         key="leaderboard"
-//         value="leaderboard"
-//         className={{ root: 'md:px-4' }}
-//       >
-//         Course Tab
-//       </Tabs.TabContent>
-//       <Tabs.TabContent
-//         key="create"
-//         value="create"
-//         className={{ root: 'md:px-4' }}
-//       >
-//         Create Tab
-//       </Tabs.TabContent>
-//     </Tabs>
-//   )
-// }
+export const Controlled = () => {
+  const [selectedTab, setSelectedTab] = useState('leaderboard')
+  return (
+    <Tabs
+      defaultValue="leaderboard"
+      value={selectedTab}
+      onValueChange={(tab) => setSelectedTab(tab)}
+      tabs={[
+        { id: 'leaderboard', value: 'leaderboard', label: 'Leaderboard' },
+        { id: 'create', value: 'create', label: 'Gruppe erstellen/beitreten' },
+      ]}
+    >
+      <Tabs.TabContent
+        key="leaderboard"
+        value="leaderboard"
+        className={{ root: 'md:px-4' }}
+      >
+        Course Tab
+      </Tabs.TabContent>
+      <Tabs.TabContent
+        key="create"
+        value="create"
+        className={{ root: 'md:px-4' }}
+      >
+        Create Tab
+      </Tabs.TabContent>
+    </Tabs>
+  )
+}
