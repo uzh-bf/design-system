c76ed548d77deae420f1b5e4616f71ca71f1db40
sjschlapbach <julius.schlapbach@gmx.net>
2025-05-27 14:58:35 +0200

    chore: fix formatting

--- packages/design-system/src/Checkbox.tsx
+++ packages/design-system/src/Checkbox.tsx
@@ -83,7 +83,7 @@ export function Checkbox({
         defaultChecked
         checked={checked || partial}
         className={twMerge(
-          'border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer h-4 w-4 shrink-0 rounded-md border focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'peer h-4 w-4 shrink-0 rounded-md border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
           (checked || partial) && 'border-black',
           disabled && 'cursor-not-allowed',
           checkboxSize[size],
