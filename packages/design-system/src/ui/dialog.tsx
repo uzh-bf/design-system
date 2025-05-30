1caeba9084a0fcabd6463c970407198408c11009
sjschlapbach <julius.schlapbach@gmx.net>
2025-05-30 20:46:44 +0200

    fix: ensure that dialog component works in combination with popover, etc.

--- packages/design-system/src/ui/dialog.tsx
+++ packages/design-system/src/ui/dialog.tsx
@@ -3,6 +3,7 @@ import { X } from 'lucide-react'
 import * as React from 'react'
 
 import { cn } from '@/lib/utils'
+import { twMerge } from 'tailwind-merge'
 
 const Dialog = DialogPrimitive.Root
 
@@ -45,12 +46,16 @@ const DialogContent = React.forwardRef<
     },
     ref
   ) => (
-    <DialogPortal>
-      <DialogOverlay className={overlayClassName} />
+    <DialogOverlay
+      className={twMerge(
+        'fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center',
+        overlayClassName
+      )}
+    >
       <DialogPrimitive.Content
         ref={ref}
         className={cn(
-          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-5 shadow-lg duration-200 sm:rounded-lg',
+          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] relative z-50 w-full max-w-lg gap-4 border bg-background p-5 shadow-lg duration-200 sm:rounded-lg',
           className
         )}
         {...props}
@@ -67,7 +72,7 @@ const DialogContent = React.forwardRef<
           </DialogPrimitive.Close>
         )}
       </DialogPrimitive.Content>
-    </DialogPortal>
+    </DialogOverlay>
   )
 )
 DialogContent.displayName = DialogPrimitive.Content.displayName
