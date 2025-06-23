998bd7e77057f80c0dfca31311adc21ad28a8edc
sjschlapbach <julius.schlapbach@gmx.net>
2025-06-23 15:44:59 +0200

    fix(*ui/dialog): make sure dialogs open from center of screen

--- packages/design-system/src/ui/dialog.tsx
+++ packages/design-system/src/ui/dialog.tsx
@@ -57,7 +57,7 @@ const DialogContent = React.forwardRef<
       <DialogPrimitive.Content
         ref={ref}
         className={cn(
-          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] bg-background relative z-50 w-full max-w-lg gap-4 border p-5 shadow-lg duration-200 sm:rounded-lg',
+          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 bg-background relative z-50 w-full max-w-lg gap-4 border p-5 shadow-lg duration-200 sm:rounded-lg',
           className
         )}
         {...props}
