134357db716b1a66dd7ab89918182ae2cc7f0c67
sjschlapbach <julius.schlapbach@gmx.net>
2025-07-03 23:43:41 +0200

    fix: ensure that preflight is correctly loaded for components in ladle

--- packages/design-system/.ladle/components.tsx
+++ packages/design-system/.ladle/components.tsx
@@ -1,6 +1,6 @@
 // organize-imports-ignore
 import type { GlobalProvider } from '@ladle/react'
 import React from 'react'
-import 'src/tailwind.css'
+import 'src/tailwind-with-preflight.css'
 
 export const Provider: GlobalProvider = ({ children }) => <>{children}</>
