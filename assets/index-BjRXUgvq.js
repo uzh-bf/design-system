42e6e5999d63f2ac772531023aa41bc8d5435f98
sjschlapbach <sjschlapbach@users.noreply.github.com>
2025-08-09 12:42:08 +0000

    deploy: 0f6f8f6a8982e5343370ccf2f7a70aef8b039d2a

--- assets/index-BjRXUgvq.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-BW6zVLxY.js";var a=globalThis!=null&&globalThis.document?t.useLayoutEffect:()=>{};export{a as u};
---
+++ assets/index-Bn24k_br.js
@@ -0,0 +1,1 @@
+import{r}from"./index-m5U6F7Ob.js";function n(t){const e=r.useRef(t);return r.useEffect(()=>{e.current=t}),r.useMemo(()=>(...s)=>{var u;return(u=e.current)==null?void 0:u.call(e,...s)},[])}export{n as u};
