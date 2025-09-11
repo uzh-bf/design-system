20606360f51ee9bc61697239e181187e7beb80cb
sjschlapbach <sjschlapbach@users.noreply.github.com>
2025-09-11 18:54:55 +0000

    deploy: 11a5e9a6f0029ac84209dfe6a240611b82c03134

--- assets/index-CjC71xm0.js
+++
@@ -1,1 +0,0 @@
-import{r as i,j as l}from"./index-DNKnBQbc.js";import{P as s}from"./index-CE6VUXtI.js";var f="Label",r=i.forwardRef((a,o)=>l.jsx(s.label,{...a,ref:o,onMouseDown:t=>{var e;t.target.closest("button, input, select, textarea")||((e=a.onMouseDown)==null||e.call(a,t),!t.defaultPrevented&&t.detail>1&&t.preventDefault())}}));r.displayName=f;var x=r;export{x as R};
---
+++ assets/index-D-E1vtMZ.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-dj-XLryl.js";var a=globalThis!=null&&globalThis.document?t.useLayoutEffect:()=>{};export{a as u};
